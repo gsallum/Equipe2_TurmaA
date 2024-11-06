ead651ce79360a2e245981797ea3ddb1a8b64c92
gsallum <gabrielsallum2006@gmail.com>
2024-11-06 08:21:38 -0300

    funcionaaqaa

---
+++ js/mistura.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Recupera 'agrupados' do localStorage e converte de volta para um array
+let agrupados = JSON.parse(localStorage.getItem('agrupados'));
+
+// Inicializa variáveis para somar os valores de R, G e B
+let totalR = 0, totalG = 0, totalB = 0;
+let totalCores = agrupados.length;
+
+// Soma os valores de cada cor
+agrupados.forEach(cor => {
+    totalR += cor[0];
+    totalG += cor[1];
+    totalB += cor[2];
+});
+
+// Calcula a média para cada componente de cor
+let mediaR = Math.round(totalR / totalCores);
+let mediaG = Math.round(totalG / totalCores);
+let mediaB = Math.round(totalB / totalCores);
+
+// Cor final da mistura
+let corMistura = `rgb(${mediaR}, ${mediaG}, ${mediaB})`;
+
+p = document.querySelector('figure p')
+p.style.backgroundColor = corMistura
+
+})
